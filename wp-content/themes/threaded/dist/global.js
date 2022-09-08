88e1f88101b22aa3b18b5244a65d1f0cdb643498
Archie Cimafranca <chientrisha@gmail.com>
2022-09-08 14:59:56 +0800

    wp version
    - wordpress

---
+++ wp-content/themes/threaded/dist/global.js
@@ -0,0 +1,18 @@
+(self["webpackChunkthreaded"] = self["webpackChunkthreaded"] || []).push([["global"],{
+
+/***/ "./assets/js/index.js":
+/*!****************************!*\
+  !*** ./assets/js/index.js ***!
+  \****************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/js/index.js"));
+/******/ }
+]);
